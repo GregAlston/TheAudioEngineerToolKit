dd3339e7b67bafbfb558f3095968ad1ef273510e
GregAlston <109775102+GregAlston@users.noreply.github.com>
2023-04-03 12:26:56 +1000

    Calculation for Comb filtering in both path length difference and arrival time difference done

--- App/TheAudioEngineerToolKit.py
+++ App/TheAudioEngineerToolKit.py
@@ -12,6 +12,10 @@ def bpm_function(bpm):
 def pld_function(path_one, path_two):
     path_result["text"]= round(344/ (((float(str(path_two))) - (float(str(path_one))))*2), 2)
 
+def atd_function(timeInput_one, timeInput_two):
+    time_result["text"]= round(1/ (((float(str(timeInput_two))) - float(str(timeInput_one)))* 2), 2)
+
+
 root = tk.Tk()
 
 
@@ -51,9 +55,9 @@ hertz_result.place(relheight=0.03, relwidth= 0.09, rely=0.193, relx=0.65)
 hertz_label = tk.Label(inside_frame, font=text_font, text="Hz", bg="white", fg="black", height=1)
 hertz_label.place(relx=0.74, rely=0.195)
 
-#Comb filtering
-comb_filter = tk.Label(inside_frame, font=header_font, text="Comb Filtering: \n Path Length Difference", bg="black", fg="white")
-comb_filter.place(relheight=0.05, relwidth=0.2, rely=0.28, relx=0.404)
+#Comb filtering Path lengh difference
+comb_filter_PLD = tk.Label(inside_frame, font=header_font, text="Comb Filtering: \n Path Length Difference", bg="black", fg="white")
+comb_filter_PLD.place(relheight=0.05, relwidth=0.2, rely=0.28, relx=0.404)
 
 #User input for Path length one
 path_one = tk.Entry(inside_frame, font=text_font, bg="white", fg="black", cursor="dot", justify="right")
@@ -64,6 +68,10 @@ path_label_one.place(relx=0.18, rely=0.365)
 
 path_meter_label1 =tk.Label(inside_frame, font =text_font, text="Metre", bg="white", fg="black")
 path_meter_label1.place(relx=0.35, rely=0.366)
+
+#Note for user
+note_PLD =tk.Label(inside_frame, font =text_font, text="Note: Path one must be the \n shortest distance from \nthe source", bg="yellow", fg="black")
+note_PLD.place(relx=0.0, rely=0.366)
 
 #User input for path length two
 path_two = tk.Entry(inside_frame, font=text_font, bg="white", fg="black", cursor="dot", justify="right")
@@ -85,6 +93,44 @@ path_result.place(relheight=0.03, relwidth= 0.09, relx=0.65, rely= 0.4)
 
 path_hertz_label =tk.Label(inside_frame, font =text_font, text="Hz", bg="white", fg="black")
 path_hertz_label.place(relx=0.74, rely=0.405)
+
+#Comb filtering ATD
+comb_filter_ATD = tk.Label(inside_frame, font=header_font, text="Comb Filtering: \n Time Arrival Difference", bg="black", fg="white")
+comb_filter_ATD.place(relheight=0.05, relwidth=0.2, rely=0.49, relx=0.404)
+
+#User input for ATD one
+timeInput_one = tk.Entry(inside_frame, font=text_font, bg="white", fg="black", cursor="dot", justify="right")
+timeInput_one.place(relheight=0.03, relwidth= 0.05, relx=0.3, rely=0.57)
+
+time_label_one = tk.Label(inside_frame, font=text_font, text="Arrival Time One", bg="white", fg="black")
+time_label_one.place(relx=0.18, rely=0.57)
+
+Time_arrival_label1 =tk.Label(inside_frame, font =text_font, text="Sec", bg="white", fg="black")
+Time_arrival_label1.place(relx=0.35, rely=0.575)
+
+#Note for user
+note_PLD =tk.Label(inside_frame, font =text_font, text="Note: Time one must be the \n shortest duration from \nthe source", bg="yellow", fg="black")
+note_PLD.place(relx=0.0, rely=0.57)
+
+#User input for ATD two
+timeInput_two = tk.Entry(inside_frame, font=text_font, bg="white", fg="black", cursor="dot", justify="right")
+timeInput_two.place(relheight=0.03, relwidth= 0.05, relx=0.3, rely=0.61)
+
+time_label_two = tk.Label(inside_frame, font=text_font, text="Arrival Time Two", bg="white", fg="black")
+time_label_two.place(relx=0.18, rely=0.61)
+
+Time_arrival_label2 =tk.Label(inside_frame, font =text_font, text="Sec", bg="white", fg="black")
+Time_arrival_label2.place(relx=0.35, rely=0.615)
+
+#Calculate ATD Button
+time_calButton = tk.Button(inside_frame, text="Calculate \n 1 / T", bg="white", fg="black", command=lambda: atd_function (timeInput_one.get(), timeInput_two.get()))
+time_calButton.place(relheight=0.05, relwidth=0.2, relx=0.404, rely= 0.589)
+
+time_result = tk.Label(inside_frame, font=text_font, bg="black", fg="white", height=1)
+time_result.place(relheight=0.03, relwidth= 0.09, relx=0.65, rely= 0.609)
+
+time_seconds_label =tk.Label(inside_frame, font =text_font, text="Seconds", bg="white", fg="black")
+time_seconds_label.place(relx=0.74, rely=0.61)
 
 root.mainloop()
 
